98b3027ed7338c7dfa07a48f443c465c1997fc0b
Blasty <79897422+BlastyTheDev@users.noreply.github.com>
2024-08-14 18:46:58 +1000

    Delete PolygonBazooka-Server directory

--- PolygonBazooka-Server/Auth/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace PolygonBazooka_Server.Auth;
-
-public class LoginRequest
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
--- PolygonBazooka-Server/Auth/RegisterRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace PolygonBazooka_Server.Auth;
-
-public class RegisterRequest
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
-}
