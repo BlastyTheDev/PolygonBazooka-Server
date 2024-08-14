54895db0d448d8171fdbafa4723864c35b3ee3ab
BlastyTheDev <rmzha2234@gmail.com>
2024-08-14 20:35:21 +1000

    added the security config

--- src/main/java/me/blasty/polygonbazookaserver/config/SecurityConfig.java
+++ src/main/java/me/blasty/polygonbazookaserver/config/SecurityConfig.java
@@ -1,15 +1,42 @@
 package me.blasty.polygonbazookaserver.config;
 
 import lombok.RequiredArgsConstructor;
+import me.blasty.polygonbazookaserver.api.security.jwt.JWTAuthFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    
+
+    private final AuthenticationProvider authProvider;
+    private final JWTAuthFilter jwtAuthFilter;
+
     private final String[] needAuthentication = {
     };
-    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authenticationProvider(authProvider)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(needAuthentication).authenticated();
+                    auth.anyRequest().permitAll();
+                })
+                .build();
+    }
+
 }
