da19275b8ad1c94790ef3c71ecef3d08807e81db
BlastyTheDev <rmzha2234@gmail.com>
2024-08-16 19:43:15 +1000

    fix forgotten compile error

--- src/main/java/me/blasty/polygonbazookaserver/config/WebSocketConfig.java
+++ src/main/java/me/blasty/polygonbazookaserver/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package me.blasty.polygonbazookaserver.config;
 
+import lombok.RequiredArgsConstructor;
 import me.blasty.polygonbazookaserver.api.multiplayer.ranked.RankedMatchHandler;
+import me.blasty.polygonbazookaserver.api.security.jwt.JWTService;
+import me.blasty.polygonbazookaserver.api.security.user.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -9,7 +12,11 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
+    
+    private final JWTService jwtService;
+    private final UserRepository userRepository;
     
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -18,7 +25,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     
     @Bean
     public RankedMatchHandler rankedMatchHandler() {
-        return new RankedMatchHandler();
+        return new RankedMatchHandler(jwtService, userRepository);
     }
     
 }
